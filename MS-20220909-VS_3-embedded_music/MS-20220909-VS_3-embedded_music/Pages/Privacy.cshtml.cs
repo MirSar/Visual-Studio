364308367006d4e784aed41d3ad1dbb0b2a94af4
Mirwais Sarwary <96671548+MirSar@users.noreply.github.com>
2022-09-10 11:20:48 -0700

    visual studio files

---
+++ MS-20220909-VS_3-embedded_music/MS-20220909-VS_3-embedded_music/Pages/Privacy.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MS_20220909_VS_3_embedded_music.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
